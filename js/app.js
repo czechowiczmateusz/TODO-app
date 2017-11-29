3a0047f59411235da0885abd4339c8cb3ea20b87
DawidWietrzynski <32936576+DawidWietrzynski@users.noreply.github.com>
2017-11-29 17:38:11 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ var form = document.querySelector("form");
 //Tablica
 var tasks = [];
 //Konstruktor Obiektu
+var see = [];
 function getID() {
     x = tasks.length;
     return x;
@@ -18,57 +19,65 @@ function getID() {
      this.priorityTask = priority;
      this.titleTask = titles;
      this.describeTask = describe;
-     console.log(tasks.length)
 }
 //Local Storage odczyt
 var task = JSON.parse(localStorage.getItem('list') );
 // Tworzenie obiektu na klik
 var button = document.getElementById("btn");
-button.addEventListener('click', function (e) {
+button.addEventListener('click', function (e){
     var taskObjects = new TasksObject();
-    tasks.push(taskObjects);
-    localStorage.setItem('list', JSON.stringify(tasks));
+    var error = false;
+    var errorMsg = document.querySelector(".error-message");
+
+    if( form.title.value.length <= 6) {
+        errorMsg.innerHTML += 'Title is too short<br>';
+        error = true;
+    }
+
+    if( form.textarea.value.length <= 20) {
+        errorMsg.innerHTML += 'Description is less than 20 characters<br>';
+        error = true;
+    }
+    if (typeof form.date.value === "string") {
+        var data = form.date.value.match(/(\d{4})-(\d{2})-(\d{2})/);
+        var thisYear = new Date().getFullYear();
+        if (data === null){
+            error = true;
+            errorMsg.innerHTML += 'You set no data<br>'
+        }
+        else if (data[1].length < 4 || data[1] < thisYear) {
+            error = true;
+            errorMsg.innerHTML += 'Wrong year<br>'
+        }
+        else if (data[2].length < 2 || data[2] < 1 || data[2] > 12){
+            error = true;
+            errorMsg.innerHTML += 'Wrong month <br>'
+        }
+        else if (data[3].length < 2 || data[3] < 1 || data[3] > 31) {
+            error = true;
+            errorMsg.innerHTML += 'Wrong day <br>'
+        }
+    }
+
+    if(error === true) {
+        e.preventDefault();
+    }
+
+    else {
+        tasks.push(taskObjects);
+        localStorage.setItem('list', JSON.stringify(tasks));
+    }
+    console.log(tasks.sort(dynamicSort("dateTask")));
 });
 
-
-
-    // Walidacja formularza
-    var errorMsg = document.querySelector('.error-message');
-
-    form.addEventListener('submit', function(e) {
-        var error = false;
-
-        // Tytuł
-        if( this.elements.title.value.length <= 6) {
-            errorMsg.innerHTML += 'Title is too short<br>';
-            error = true;
-        };
-
-        // Opis
-        if( this.elements.textarea.value.length <= 20) {
-            errorMsg.innerHTML += 'Description is less than 20 characters<br>';
-            error = true;
-        };
-
-        if(error) {
-            e.preventDefault();
-        }
-    });
-
-    // Aktualna data
-    var today = new Date();
-    var dd = today.getDate();
-    var mm = today.getMonth()+1;
-    var yyyy = today.getFullYear();
-    var date = document.getElementById("date");
-
-    if(dd<10) {
-        dd = '0'+dd
+function dynamicSort(property) {
+    var sortOrder = 1;
+    if(property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
     }
-
-    if(mm<10) {
-        mm = '0'+mm
+    return function (a,b) {
+        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+        return result * sortOrder;
     }
-
-    today = yyyy + "-" + mm + "-" + dd;
-    date.value += today;
+}
